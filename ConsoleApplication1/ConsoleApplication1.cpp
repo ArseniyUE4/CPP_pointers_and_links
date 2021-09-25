d1e30f44877bab959b8eb23311ee6edbbb9bb7d7
ArseniyUE4 <91388672+ArseniyUE4@users.noreply.github.com>
2021-09-25 22:43:36 +0300

    ArseniyUE4

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std
+
+//pointers
+int *num = new int(5); //pointer to memory cell  number
+int *nums = new int[5]; //pointer to memory cell  array
+
+//links
+const int number = 12;
+
+int& link = *num;
+const int& link2 = number;
+
+//how to use
+
+int val = 12;
+
+void func(int& val) {
+	val += 1;
+}
+
+int main() {
+	cout << "Before: " << val << endl;
+	int& r_val = val;
+	func(r_val);
+	cout << "After: " << val;
+}
